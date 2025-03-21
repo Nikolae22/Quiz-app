01b00e43ec24e0fa746de450b1635ddf6d94bc28
Nicolae Moraru <morarunicolae96@gmail.com>
2025-03-21 11:03:49 +0100

    question timer components inizialization

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,15 @@
 import {useState} from "react";
 
 import QUESTIONS from '../data/questions.js'
+import QuestionTimer from "./QuestionTimer.jsx";
+
 
 export default function Quiz() {
-
 
     const [userAnswers, setUserAnswers] = useState([])
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevState) => {
@@ -19,13 +21,25 @@ export default function Quiz() {
 
     }
 
+    if (quizIsComplete) {
+        return <div id='summary'>
+            <img src="../assets/quiz-complete.png" alt="Quiz complete"/>
+            <h2>Quiz complete</h2>
+        </div>
+    }
+
+    const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffleAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
             <div id="questions">
+                <QuestionTimer
+                    timeout={10000}
+                    onTimeout={() => handleSelectAnswer(null)}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id='answers'>
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffleAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}
                             >{answer}</button>
