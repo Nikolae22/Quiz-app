a51f4b4195d707fe966be5a22fb75601d33b66da
Nicolae Moraru <morarunicolae96@gmail.com>
2025-03-21 10:26:48 +0100

    quiz component in working

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header(){
+
+
+    return(
+        <headr>
+            <img src="../assets/quiz-logo.png" alt="Quiz logo"/>
+            <h1>ReactQuiz</h1>
+        </headr>
+    )
+}
